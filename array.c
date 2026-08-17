9b9f0fec87159fe0ad5b10057dd51fafb4f9c592
BeamBiie <tanapong151147@gmail.com>
2026-08-17 09:42:50 +0700

    Add files via upload

---
+++ array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int score[4] = { 50,65,80,90 };
+    printf("score[0] = %d\n", score[0]);
+    printf("score[1] = %d\n", score[1]);
+    printf("score[2] = %d\n", score[2]);
+    printf("score[3] = %d\n", score[3]);
+    printf("score[4] = %d\n", score[4]);
+}
